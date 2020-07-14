9d9f8bb44eaa8f8e9e95e6ecdd64ccf5206aff8e
Mohit Bhole <43913328+moiiiiit@users.noreply.github.com>
2020-07-14 05:10:01 -0500

    Updates

---
+++ precache-manifest.462af8061a591f4a5a89dc8754313b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c49bcf8c3416bf88b9088a7cddc653",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1521890ca017d8e5492d",
+    "url": "/my-portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "c3a7daf8c9a36e820a75",
+    "url": "/my-portfolio/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "1521890ca017d8e5492d",
+    "url": "/my-portfolio/static/js/2.51242b6d.chunk.js"
+  },
+  {
+    "revision": "c3a7daf8c9a36e820a75",
+    "url": "/my-portfolio/static/js/main.fa4e991f.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  }
+]);
